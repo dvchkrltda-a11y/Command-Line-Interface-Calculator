3ed16cd7b3c9d73eb4c25cfcd310cc295f274fff
Deflo <deflotengker026@student.unsrat.ac.id>
2025-11-26 08:55:22 +0700

    Mengembangkan Fitur Pemilihan Operasi

--- Command-Line-Interface-Calculator.py
+++ Command-Line-Interface-Calculator.py
@@ -23,3 +23,14 @@ pilihan = input("Masukkan pilihan (1/2/3/4): ")
 
 angka1 = float(input("Masukkan angka pertama: "))
 angka2 = float(input("Masukkan angka kedua: "))
+
+if pilihan == "1":
+    print("Hasil:", tambah(angka1, angka2))
+elif pilihan == "2":
+    print("Hasil:", pengurangan(angka1, angka2))
+elif pilihan == "3":
+    print("Hasil:", kali(angka1, angka2))
+elif pilihan == "4":
+    print("Hasil:", pembagian(angka1, angka2))
+else:
+    print("Pilihan tidak valid!")
