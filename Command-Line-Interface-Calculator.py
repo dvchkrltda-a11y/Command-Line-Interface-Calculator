516532fdf2ac60df877e0dfa5775cacca39b9d68
Diva <divalatada026@student.ac.id>
2025-11-26 09:42:46 +0700

    fitur perkalian

--- Command-Line-Interface-Calculator.py
+++ Command-Line-Interface-Calculator.py
@@ -5,12 +5,11 @@ def tambah(a, b):
 def pengurangan(a, b):
     return a - b
 
-print("Hasil:", pengurangan(15, 6))
+def kali(a, b):
+    return a * b
 
 def pembagian(a, b):
     if b == 0:
         return "Error: Tidak bisa membagi dengan nol!"
     return a / b
 
-print(pembagian(10, 2))
-print(pembagian(8, 0))
