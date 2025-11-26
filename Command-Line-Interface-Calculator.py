1863309885ce62d6b2233474538cff02ca5ce098
Debora <deboraalalinti026@student.ac.id>
2025-11-26 09:16:47 +0800

    menambahkan fitur pembagian

--- Command-Line-Interface-Calculator.py
+++ Command-Line-Interface-Calculator.py
@@ -3,3 +3,10 @@ def pengurangan(a, b):
 
 print("Hasil:", pengurangan(15, 6))
 
+def pembagian(a, b):
+    if b == 0:
+        return "Error: Tidak bisa membagi dengan nol!"
+    return a / b
+
+print(pembagian(10, 2))
+print(pembagian(8, 0))
